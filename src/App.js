28ef4079c5e524745687582cf3ee948db549876a
miguelAngel <miguelangelhernandezgarcia@ieselrincon.es>
2023-05-13 12:45:08 +0100

    RS repaired

--- src/App.js
+++ src/App.js
@@ -4,7 +4,6 @@ import Home from "./pages/home/Home";
 import Contact from "./pages/contact/Contact";
 import Map from "./pages/Map/Map";
 import ClientData from "./pages/ClientData/clientData";
-import RSS from "../src/pages/RSS/rss.js";
 
 
 function App() {
@@ -16,7 +15,6 @@ function App() {
         <Route path="/contact" element={<Contact />}/>
         <Route path="/Map" element={<Map />}/>
         <Route path="/ClientData" element={<ClientData />}/>
-        <Route path="/RSS" element={<RSS />}/>
       </Routes>
     </BrowserRouter>
   );
--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,10 +1,12 @@
 import "./Header.css";
+import RSS from "../../services/rssData.xml";
 
 function Header() {
   return (
     <>
     <img className="Logo" src="\assets\img\icon-harman.png" alt="Logo" />
       <ul className="headerUl">
+        <li><ion-icon name="logo-rss"></ion-icon><a href={RSS} target="_blank">RSS</a></li>
         <li><a href="/Home">Home</a></li>
         <li><a href="/contact">Contacto</a></li> 
         <li><a href="/Map">Mapa</a></li>
